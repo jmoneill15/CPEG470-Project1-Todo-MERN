117590ecfe9b63e9ecef48e676e95bab34aa5732
jmoneill15 <jmoneill@udel.edu>
2024-10-22 22:24:13 -0400

    added front end

--- backend/index.js
+++ backend/index.js
@@ -2,91 +2,30 @@ import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Todo } from "./models/todo.js";
+import todoRoute from "./routes/todoRoutes.js";
+import cors from 'cors';
 
 const app = express();
 
-//Parsing request body
+//Middleware Parsing request body
 app.use(express.json());
+
+//Middleware handles CORS POLICT
+app.use(cors({
+    origin: 'https://localhost:3000',//front end url
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+}
+));
 
 app.get('/', (request, response)=>{
     console.log(request)
     return response.status(234).send("welcome to tutorial")
 });
+
+app.use('/todo', todoRoute);
+
 //7SmuLaf1TsN8OV6I
-
-//Route for saving a new Todo Task
-app.post('/todo', async (request, response)=>{
-    try{
-        if(
-            !request.body.task
-        ){
-            return response.status(400).send({
-                message: "Send all required fields: task",
-            })
-        }
-        const newTask = {
-            task: request.body.task
-        };
-        const task = await Todo.create(newTask);
-        return response.status(201).send(task);
-    }catch(error){
-        console.log(error.message);
-        response.status(500).send({message: error.message});
-    }
-});
-
-
-//Route to get all tasks from database
-app.get('/todo', async (request, response)=>{
-    try{
-        const todos = await Todo.find({});
-        return response.status(200).json({
-            count: todos.length,
-            data: todos
-        });
-    }
-    catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message})
-    }
-});
-
-//Route to get one task from database by id
-app.get('/todo/:id', async (request, response)=>{
-    try{
-        const{ id } = request.params;
-
-        const todo = await Todo.findById(id);
-        return response.status(200).json(todo);
-    }
-    catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message})
-    }
-});
-
-
-//Route to update a task
-app.put('/todo/:id', async (request, response)=>{
-    try{
-        if (!request.body.task){
-            return response.status(400).send({
-                message: 'Send all required fields: task'
-            })
-        }
-    const {id} = request.params;
-    const result = await Todo.findByIdAndUpdate(id, request.body);
-
-    if(!result){
-        return response.status(400).send({message: 'Todo not found'})
-    }
-    return response.status(200).send({message: 'Todo task updated successfully'})
-    }catch (error){
-        console.log(error.message);
-        response.status(500).send({message: error.message})
-    }
-});
-
 
 mongoose
 .connect(mongoDBURL)
---
+++ backend/routes/todoRoutes.js
@@ -0,0 +1,96 @@
+import express from "express";
+import { Todo } from "../models/todo.js";
+
+const router = express.Router();
+
+//Route for saving a new Todo Task
+router.post('/', async (request, response)=>{
+    try{
+        if(
+            !request.body.task
+        ){
+            return response.status(400).send({
+                message: "Send all required fields: task",
+            })
+        }
+        const newTask = {
+            task: request.body.task
+        };
+        const task = await Todo.create(newTask);
+        return response.status(201).send(task);
+    }catch(error){
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
+
+
+//Route to get all tasks from database
+router.get('/', async (request, response)=>{
+    try{
+        const todos = await Todo.find({});
+        return response.status(200).json({
+            count: todos.length,
+            data: todos
+        });
+    }
+    catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+//Route to get one task from database by id
+router.get('/:id', async (request, response)=>{
+    try{
+        const{ id } = request.params;
+
+        const todo = await Todo.findById(id);
+        return response.status(200).json(todo);
+    }
+    catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+
+//Route to update a task
+router.put('/:id', async (request, response)=>{
+    try{
+        if (!request.body.task){
+            return response.status(400).send({
+                message: 'Send all required fields: task'
+            })
+        }
+    const {id} = request.params;
+    const result = await Todo.findByIdAndUpdate(id, request.body);
+
+    if(!result){
+        return response.status(400).send({message: 'Todo not found'})
+    }
+    return response.status(200).send({message: 'Todo task updated successfully'})
+    }catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+//Route for deleting a todo task
+router.delete('/:id', async (request, response)=>{
+    try{
+        const {id} = request.params;
+        const result = await Todo.findByIdAndDelete(id);
+
+        if(!result){
+            return response.status(400).send({message: 'Todo not found'})
+        }
+        return response.status(200).send({message: 'Todo task deleted successfully'})
+
+    }catch (error){
+        console.log(error.message);
+        response.status(500).send({message: error.message})
+    }
+});
+
+export default router;
